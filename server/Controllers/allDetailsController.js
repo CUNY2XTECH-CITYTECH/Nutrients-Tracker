66bb56b90f39c0a43ecaf5f249b70f6bd3f60ad7
Syed Asghar <syedasghar178@gmail.com>
2025-08-21 13:10:28 -0400

    Save food details to redis cache

--- server/Controllers/allDetailsController.js
+++ server/Controllers/allDetailsController.js
@@ -1,8 +1,20 @@
 import axios from "axios";
+import Redis from "redis";
 import dotenv from "dotenv";
 dotenv.config();
 
 const API_KEY = process.env.USDA_API_KEY;
+const redisClient = Redis.createClient('redis://localhost:6379'); //pass production instance url for redis when going into production
+
+// IIFE (anonymous async function that run whenever the file runs) Connect to Redis when the app starts
+(async () => {
+  try {
+    await redisClient.connect();
+    console.log('Redis connected successfully');
+  } catch (error) {
+    console.error('Redis connection failed:', error);
+  }
+})();
 
 
 // The object format that is going to be sent to the server
@@ -45,11 +57,8 @@ const foodTemplate = {
 };
 
 
-
-
-
 //Function adds the data of the nutrients to the object
-function filterFoodData(foodData) {
+async function filterFoodData(foodData) {
 
   const result = JSON.parse(JSON.stringify(foodTemplate)); //modify a copy of foodTemple convert into json and then js object to make deep copy
   
@@ -66,13 +75,13 @@ function filterFoodData(foodData) {
       result.nutrients[nutrientId] = nutrientValue;
     }
   });
-
+  await redisClient.json.set(`foodId:${result.foodId}`, "$", result); //Saves food details to redis cache
   return result;
 }
 
-export async function FoodDetails(req, res) {
-  const foodId = req.params.foodId || req.query.foodId;
 
+//Makes a API resquest to USDA api to get food data and then saves it to redis cache
+async function usdaApiRequest(foodId) {
   try {
     const response = await axios.get(`https://api.nal.usda.gov/fdc/v1/food/${foodId}`, {
       params: { 
@@ -80,12 +89,44 @@ export async function FoodDetails(req, res) {
         dataType: "Survey (FNDDS)" //Only gives results that have the object dataType:"Survey (FNDDS)". 
       }
       });
-    
-    const filteredData = filterFoodData(response.data);
-    res.json(filteredData);
+
+    return response.data
     
   } catch(error) {
     console.error(error);
-    res.status(500).json({ error: "Failed to fetch food details" });
+    throw error
   }
+}
+
+
+//Gets the food details saved in cache by foodID. If its not there, return null
+async function redisCacheRequest (foodId) {
+
+  try {
+    const getCache = await redisClient.json.get(`foodId:${foodId}`)
+    console.log(getCache)
+    return getCache
+
+  }catch(error){
+    console.error(error)
+    return null
+  }
+}
+
+
+//This is the funciton that is getting exported and receives the req and res arguements from routes
+export async function FoodDetails(req, res) {
+  const foodId = req.params.foodId || req.query.foodId;  //Gets the foodId that is send by client
+
+  const detailsFromCache = await redisCacheRequest(foodId)  //Runs function that gets foodDetails from cache
+
+  //If not in cache, makes api call to USDA API to get it and then fillers data to what we need to send to client
+  if (!detailsFromCache) {
+    const detailsFromApi = await usdaApiRequest(foodId)
+    const formatFoodInfo = await filterFoodData(detailsFromApi)
+    res.json(formatFoodInfo)    
+  }
+
+  //If in cache, return it to client
+  res.json(detailsFromCache)
 }
