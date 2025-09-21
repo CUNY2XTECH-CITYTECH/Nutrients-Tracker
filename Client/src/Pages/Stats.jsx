00a43fcf03d49fa709f79524a5b9c8a0d76c3709
thatguy4568 <norm.miller1@gmail.com>
2025-09-21 11:51:27 -0400

    Changes to the stats page

--- Client/src/Pages/Stats.jsx
+++ Client/src/Pages/Stats.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Pie, Doughnut } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
